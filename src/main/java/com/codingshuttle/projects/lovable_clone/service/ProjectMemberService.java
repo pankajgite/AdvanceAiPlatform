98daaa1e5b0ab7791c111db2c89f13a1a99ab0ed
pankajgite <pankaj.gite2021@gmail.com>
2026-01-16 00:12:30 +0530

    module 4 live 2 1:30

--- src/main/java/com/codingshuttle/projects/lovable_clone/service/ProjectMemberService.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/service/ProjectMemberService.java
@@ -7,11 +7,11 @@ import com.codingshuttle.projects.lovable_clone.dto.member.UpdateMemberRoleReque
 import java.util.List;
 
 public interface ProjectMemberService {
-    List<MemberResponse> getProjectMembers(Long projectId, Long userId);
+    List<MemberResponse> getProjectMembers(Long projectId);
 
-    MemberResponse inviteMember(Long projectId, InviteMemberRequest request, Long userId);
+    MemberResponse inviteMember(Long projectId, InviteMemberRequest request);
 
-    void removeProjectMember(Long projectId, Long memberId, Long userId);
+    void removeProjectMember(Long projectId, Long memberId);
 
-    MemberResponse updateMemberRole(Long projectId, Long memberId, UpdateMemberRoleRequest request, Long userId);
+    MemberResponse updateMemberRole(Long projectId, Long memberId, UpdateMemberRoleRequest request);
 }
